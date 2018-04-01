6206a1a2b0511c07d25bc1183f46b7f1a560da6e
kizz <zp.zoki95@gmail.com>
2018-04-01 20:27:44 +0200

    added app

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,14 +4,15 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+# import hello.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    # url(r'^$', hello.views.index, name='index'),
+    # url(r'^db', hello.views.db, name='db'),
+    path('', include('olr.urls')),
     path('admin/', admin.site.urls),
 ]
