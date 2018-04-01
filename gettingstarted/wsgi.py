5cf1c262522eddd12c3841735e1ebd75f1f784b3
kizz <zp.zoki95@gmail.com>
2018-04-01 20:12:01 +0200

    start app

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -1,16 +1,15 @@
 """
-WSGI config for OLR_Web_Site project.
+WSGI config for gettingstarted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OLR_Web_Site.settings")
 
 application = get_wsgi_application()
